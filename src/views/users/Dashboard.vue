d4c6a2051885602f9a91ed273565b85f8459264f
ryan <ball.ryan.rb@gmail.com>
2021-11-04 19:43:45 -0600

    adds computed functions for net_cash,etc. calculations instead of having all of the logic in the template section

--- src/views/users/Dashboard.vue
+++ src/views/users/Dashboard.vue
@@ -1,124 +1,127 @@
 <template>
   <div>
-    Year:
-    <select id="year" name="year" v-model="yearFilter">
-      <option value="2021">2021</option>
-      <option value="2022">2022</option>
-      <option value="2023">2023</option>
-      <option value="2024">2024</option>
-      <option value="2025">2025</option>
-      <option value="2026">2026</option>
-      <option value="2027">2027</option>
-      <option value="2028">2028</option>
-      <option value="2029">2029</option>
-      <option value="2030">2030</option>
-    </select>
-    Month:
-    <select id="month" name="month" v-model="monthFilter">
-      <option value="01">January</option>
-      <option value="02">February</option>
-      <option value="03">March</option>
-      <option value="04">April</option>
-      <option value="05">May</option>
-      <option value="06">June</option>
-      <option value="07">July</option>
-      <option value="08">August</option>
-      <option value="09">September</option>
-      <option value="10">October</option>
-      <option value="11">November</option>
-      <option value="12">December</option>
-    </select>
-    <h3>Activity</h3>
-    <p>
-      <strong>Total Expenses:</strong>
-      {{ monthlyExpenses.total }}
-    </p>
-    <p>
-      <strong>Total Incomes:</strong>
-      {{ monthlyIncomes.total }}
-    </p>
-    <p>
-      <strong>Net Change:</strong>
-      {{ netChange(monthlyIncomes.total, monthlyExpenses.total) }}
-    </p>
-    <hr />
-    <h3>Cash Available</h3>
-    <p>
-      <strong>Net Cash Beginning:</strong>
-      <!-- Equal to the net cash available at the start of the current month -->
-      {{
-        monthlyNetCash
-          .filter((balance) => balance.year == yearFilter)
-          .filter((currentYearBalance) => currentYearBalance.month == monthFilter)[0].net_cash
-      }}
-    </p>
-    <form v-on:submit.prevent="currentBalanceCreate()">
-      <h3>New Current Balance</h3>
-      <ul>
-        <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
-      </ul>
-      <div>
-        <label>Checking 1:</label>
-        <input type="text" v-model="currentBalanceParams.checking1" />
-      </div>
-      <div>
-        <label>Checking 2:</label>
-        <input type="text" v-model="currentBalanceParams.checking2" />
-      </div>
-      <div>
-        <label>Savings 1:</label>
-        <input type="text" v-model="currentBalanceParams.savings1" />
-      </div>
-      <div>
-        <label>Savings 2:</label>
-        <input type="text" v-model="currentBalanceParams.savings2" />
-      </div>
-      <div>
-        <label>Credit Card 1:</label>
-        <input type="text" v-model="currentBalanceParams.credit_card1" />
-      </div>
-      <div>
-        <label>Credit Card 2:</label>
-        <input type="text" v-model="currentBalanceParams.credit_card2" />
-      </div>
-      <div>
-        <label>Credit Card 3:</label>
-        <input type="text" v-model="currentBalanceParams.credit_card3" />
-      </div>
-      <div>
-        <label>Credit Card 4:</label>
-        <input type="text" v-model="currentBalanceParams.credit_card4" />
-      </div>
-      <input type="submit" value="Submit" />
-    </form>
-    <p>
-      <strong>Net Cash Current:</strong>
-      {{ currentBalance.net_cash }}
-    </p>
-    <p>
-      <strong>Difference:</strong>
-      <!-- {{ cashDifference(currentBalance.net_cash, monthlyNetCash[monthlyNetCash.length - 1][1]) }} -->
-    </p>
-    <p>
-      <strong>Transactions Missing:</strong>
-      <!-- {{
-        transactionsMissing(
-          cashDifference(currentBalance.net_cash, monthlyNetCash[monthlyNetCash.length - 1][1]),
-          netChange(monthlyIncomes.total, monthlyExpenses.total)
-        )
-      }} -->
-    </p>
-    <!-- {{ categoryExpenses }}
+    <button v-on:click="currentDate()" v-if="!monthFilter">Show Dashboard</button>
+    <div v-if="monthFilter">
+      <span>
+        Year:
+        <select id="year" name="year" v-model="yearFilter">
+          <option value="2021">2021</option>
+          <option value="2022">2022</option>
+          <option value="2023">2023</option>
+          <option value="2024">2024</option>
+          <option value="2025">2025</option>
+          <option value="2026">2026</option>
+          <option value="2027">2027</option>
+          <option value="2028">2028</option>
+          <option value="2029">2029</option>
+          <option value="2030">2030</option>
+        </select>
+      </span>
+      <span>
+        Month:
+        <select id="month" name="month" v-model="monthFilter">
+          <option value="01">January</option>
+          <option value="02">February</option>
+          <option value="03">March</option>
+          <option value="04">April</option>
+          <option value="05">May</option>
+          <option value="06">June</option>
+          <option value="07">July</option>
+          <option value="08">August</option>
+          <option value="09">September</option>
+          <option value="10">October</option>
+          <option value="11">November</option>
+          <option value="12">December</option>
+        </select>
+      </span>
+      <h3>Activity</h3>
+      <p>
+        <strong>Total Expenses:</strong>
+        {{ computedExpense }}
+      </p>
+      <p>
+        <strong>Total Incomes:</strong>
+        {{ computedIncome }}
+      </p>
+      <p>
+        <strong>Net Change:</strong>
+        {{ netChange(computedIncome, computedExpense) }}
+      </p>
+      <hr />
+      <h3>Cash Available</h3>
+      <p>
+        <strong>Net Cash Beginning:</strong>
+        <!-- Equal to the net cash available at the start of the current month -->
+        {{ computedNetCash.net_cash }}
+      </p>
+      <form v-on:submit.prevent="currentBalanceCreate()">
+        <h3>New Current Balance</h3>
+        <ul>
+          <li v-for="error in errors" v-bind:key="error">{{ error }}</li>
+        </ul>
+        <div>
+          <label>Checking 1:</label>
+          <input type="text" v-model="currentBalanceParams.checking1" />
+        </div>
+        <div>
+          <label>Checking 2:</label>
+          <input type="text" v-model="currentBalanceParams.checking2" />
+        </div>
+        <div>
+          <label>Savings 1:</label>
+          <input type="text" v-model="currentBalanceParams.savings1" />
+        </div>
+        <div>
+          <label>Savings 2:</label>
+          <input type="text" v-model="currentBalanceParams.savings2" />
+        </div>
+        <div>
+          <label>Credit Card 1:</label>
+          <input type="text" v-model="currentBalanceParams.credit_card1" />
+        </div>
+        <div>
+          <label>Credit Card 2:</label>
+          <input type="text" v-model="currentBalanceParams.credit_card2" />
+        </div>
+        <div>
+          <label>Credit Card 3:</label>
+          <input type="text" v-model="currentBalanceParams.credit_card3" />
+        </div>
+        <div>
+          <label>Credit Card 4:</label>
+          <input type="text" v-model="currentBalanceParams.credit_card4" />
+        </div>
+        <input type="submit" value="Submit" />
+      </form>
+      <p>
+        <strong>Net Cash Current:</strong>
+        {{ currentBalance.net_cash }}
+      </p>
+      <p>
+        <strong>Difference:</strong>
+        {{ cashDifference(currentBalance.net_cash, computedNetCash.net_cash) }}
+      </p>
+      <p>
+        <strong>Transactions Missing:</strong>
+        {{
+          transactionsMissing(
+            cashDifference(currentBalance.net_cash, computedNetCash.net_cash),
+            netChange(computedIncome, computedExpense)
+          )
+        }}
+      </p>
+      <!-- {{ categoryExpenses }}
     <div v-for="category in categoryExpenses.categoryExpenses" v-bind:key="category">
       <p>{{ category }}</p>
     </div> -->
-    <h1>Expenses</h1>
-    <div v-for="expense in expenses" v-bind:key="`${expense.id}A`">
-      <p>{{ expense.date }} {{ expense.category }} {{ expense.amount }}</p>
-    </div>
-    <h1>Incomes</h1>
-    <div v-for="income in incomes" v-bind:key="`${income.id}B`">
-      <p>{{ income.date }} {{ income.category }} {{ income.amount }}</p>
+      <h1>Expenses</h1>
+      <div v-for="expense in expenses" v-bind:key="`${expense.id}A`">
+        <p>{{ expense.date }} {{ expense.category }} {{ expense.amount }}</p>
+      </div>
+      <h1>Incomes</h1>
+      <div v-for="income in incomes" v-bind:key="`${income.id}B`">
+        <p>{{ income.date }} {{ income.category }} {{ income.amount }}</p>
+      </div>
     </div>
   </div>
 </template>
@@ -137,12 +140,13 @@ export default {
       categoryExpenses: [],
       monthlyIncomes: [],
       categoryIncomes: [],
-      monthlyNetCash: [{ net_cash: 0 }],
+      monthlyNetCash: [],
+      selectedNetCash: {},
       currentBalance: {},
       currentBalanceParams: {},
       errors: [],
-      yearFilter: new Date().getFullYear(),
-      monthFilter: new Date().getMonth() + 1,
+      yearFilter: 0,
+      monthFilter: 0,
     };
   },
   created: function () {
@@ -179,6 +183,19 @@ export default {
       this.currentBalance = response.data;
     });
   },
+  computed: {
+    computedNetCash: function () {
+      return this.monthlyNetCash
+        .filter((balance) => balance.year == this.yearFilter)
+        .filter((currentYearBalance) => currentYearBalance.month == this.monthFilter)[0];
+    },
+    computedIncome: function () {
+      return this.monthlyIncomes.monthly_incomes[this.monthFilter];
+    },
+    computedExpense: function () {
+      return this.monthlyExpenses.monthly_expenses[this.monthFilter];
+    },
+  },
   methods: {
     netChange: (income, expense) => {
       return (income - expense).toFixed(2);
@@ -203,6 +220,10 @@ export default {
     },
     clearCurrentBalanceParams: function () {
       this.currentBalanceParams = {};
+    },
+    currentDate: function () {
+      this.yearFilter = new Date().getFullYear();
+      this.monthFilter = new Date().getMonth() + 1;
     },
   },
 };
